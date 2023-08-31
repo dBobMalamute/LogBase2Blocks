b6648d52e40589d90aacf20c631c1f85deaab291
David R Meldgin <davidrmeldgin@gmail.com>
2023-08-30 21:22:07 -0500

    Got everything Together

---
+++ Plugin/Plugin.cpp
@@ -0,0 +1,49 @@
+#include "Plugin.h"
+
+#include "Blank.h"
+
+#include "NumberDataType.h"
+#include "NumberDisplayIdea.h"
+#include "TileLogarithmBase2Idea.h"
+#include "NumberInputIdea.h"
+
+QString Plugin::PluginName()
+{
+    return "PLUGIN";
+}
+
+QStringList Plugin::authors()
+{
+    return QStringList("dBob");
+}
+
+QString Plugin::PluginDescription()
+{
+    return "";
+}
+
+void Plugin::registerIdeas(std::shared_ptr<IdeaRegistry> ideaRegistry)
+{
+    ideaRegistry->registerIdea<NumberDisplayIdea>();
+    ideaRegistry->registerIdea<TileLogarithmBase2Idea>();
+    ideaRegistry->registerIdea<NumberInputIdea>();
+
+    qmlRegisterType(QUrl("qrc:/QML/QMLTemplates/StatisticTemplate.qml"), "com.malamute.numbers", 1, 0, "StatisticTemplate");
+    qmlRegisterType(QUrl("qrc:/QML/QMLTemplates/OneToOneTemplate.qml"), "com.malamute.calculator", 1, 0, "OneToOneTemplate");
+    qmlRegisterType(QUrl("qrc:/QML/QMLTemplates/LogarithmTemplate.qml"), "com.malamute.calculator", 1, 0, "LogarithmTemplate");
+}
+
+void Plugin::registerDataTypeAttributes(std::shared_ptr<DataTypeRegistry> dataTypeRegistry)
+{
+    dataTypeRegistry->registerDataTypeAttributes<NumberDataTypeAttributes>();
+}
+
+void Plugin::registerToolbars(std::shared_ptr<ToolbarRegistry> toolbarRegistry)
+{
+    QStringList orderList;
+    orderList << Magic<NumberInputAttributes>::cast().listName();
+    orderList << Magic<TileLogarithmBase2Attributes>::cast().listName();
+    orderList << Magic<NumberDisplayAttributes>::cast().listName();
+
+    toolbarRegistry->registerToolbar(PluginName(),orderList);
+}
